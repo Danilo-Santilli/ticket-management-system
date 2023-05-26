0858c2b74052c2bd2bbdcef58e89026bd11e732a
Danilo Santilli <danilo-santilli@hotmail.com>
2023-05-26 04:00:37 +0100

    Logando o usuário

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,6 @@
 import  { useState, createContext, useEffect } from 'react';
 import { auth, db } from '../services/firebaseConnection';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -13,9 +13,41 @@ function AuthProvider({children}){
 
     const navigate = useNavigate();
 
-    function signIn(email, password){
-        alert('ok');
+    async function signIn(email, password){
+        setLoadingAuth(true);
+    
+        // Faz o login do usuário usando o email e a senha fornecidos
+        await signInWithEmailAndPassword(auth, email, password)
+        .then(async (value)=>{
+            let uid = value.user.uid;
+    
+            // Obtém os dados do usuário do Firestore usando o ID do usuário
+            const docRef = doc(db, 'users', uid);
+            const docSnap = await getDoc(docRef);
+    
+            // Cria um objeto de dados do usuário com as informações necessárias
+            let data = {
+                uid: uid,
+                nome: docSnap.data().nome,
+                email: value.user.email,
+                avatarUrl: docSnap.data().avatarUrl
+            }
+    
+            // Define o usuário autenticado no estado e no armazenamento local
+            setUser(data);
+            storageUser(data);
+    
+            setLoadingAuth(false);
+            toast.success('Você entrou no sistema!');
+            navigate('/dashboard');
+        })
+        .catch((error)=>{
+            console.log(error);
+            setLoadingAuth(false);
+            toast.error('Ops, algo deu errado!');
+        })
     }
+    
 
     async function signUp(name, email, password){
         setLoadingAuth(true);
@@ -46,6 +78,7 @@ function AuthProvider({children}){
         .catch((error)=>{
             console.log(error);
             setLoadingAuth(false);
+            toast.error('Ops, algo deu errado!');
         });
     }
 
