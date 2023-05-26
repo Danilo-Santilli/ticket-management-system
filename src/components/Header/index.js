c0feb61cdfceee27582f3ddc74a85d3fd03249f0
Danilo Santilli <danilo-santilli@hotmail.com>
2023-05-26 05:01:54 +0100

    Criando o header

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import avatarImg from '../../assets/avatar.png';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
+import './header.css';
+
+export default function Header(){
+    const { user } = useContext(AuthContext);
+    return(
+        <header className="sidebar">
+            <div>
+                <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt="Foto do usuário" />
+            </div>
+
+            <Link to='/dashboard'>
+                <FiHome color='#fff' size={24}/>
+                Chamados
+            </Link>
+            <Link to='/costumers'>
+                <FiUser color='#fff' size={24}/>
+                Clientes
+            </Link>
+            <Link to='/profile'>
+                <FiSettings color='#fff' size={24}/>
+                Perfil
+            </Link>
+        </header>
+    )
+}
