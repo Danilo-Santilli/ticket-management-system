24fbe0822d9bb6b582b84ec22a94597bef0abf76
Danilo Santilli <danilo-santilli@hotmail.com>
2023-06-04 00:49:12 +0100

    Cadastrando clientes no banco de dados

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
-import Costumers from '../pages/Costumers';
+import Customers from '../pages/Customers';
 
 import Private from './Private';
 
@@ -15,7 +15,7 @@ function RoutesApp(){
             <Route path='/register' element={<SignUp/>}/>
             <Route path='/dashboard' element={<Private><Dashboard/></Private>}/>
             <Route path='/profile' element={<Private><Profile/></Private>}/>
-            <Route path='/costumers' element={<Private><Costumers/></Private>}/>
+            <Route path='/customers' element={<Private><Customers/></Private>}/>
         </Routes>
     )
 }
