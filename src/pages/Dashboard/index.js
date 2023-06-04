d4f676ce894ef54b9e11ec845793c7967c986922
Danilo Santilli <danilo-santilli@hotmail.com>
2023-06-04 01:26:11 +0100

    Criando Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,10 @@
 import { AuthContext } from "../../contexts/auth";
 import { useContext } from "react";
 import Header from "../../components/Header";
+import Title from '../../components/Title';
+import './dashboard.css';
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 export default function Dashboard(){
     const { logout } = useContext(AuthContext);
@@ -12,8 +16,46 @@ export default function Dashboard(){
     return(
         <div>
             <Header/>
-            <h1>Dashboard</h1>
-            <button onClick={handleLogout}>Sair</button>
+            <div className="content">
+                <Title name='Tickets'>
+                    <FiMessageSquare size={25}/>
+                </Title>
+                
+                <>
+                    <Link className="new" to='/new'>
+                        <FiPlus color="#fff" size={25}/>
+                        Novo chamado
+                    </Link>
+                    
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tBody>
+                            <tr>
+                                <td data-Label='Cliente'>empresa 1</td>
+                                <td data-Label='Assunto'>****</td>
+                                <td data-Label='Status'>****</td>
+                                <td data-Label='Cadastrado'>****</td>
+                                <td data-Label='#'>
+                                    <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                       <FiSearch color="#fff" size={17}/>
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#f6a935'}}>
+                                       <FiEdit2 color="#fff" size={17}/>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tBody>
+                    </table>
+                </>
+            </div>
         </div>
     )
 }
