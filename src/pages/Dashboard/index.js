36084b696145d2b17f518eafb8df35f1cc5c5e1b
Danilo Santilli <danilo-santilli@hotmail.com>
2023-06-05 02:39:34 +0100

    Buscando chamados pt2

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -7,6 +7,7 @@ import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { db } from "../../services/firebaseConnection";
 import { getDocs, collection, orderBy, limit, startAfter, query } from "firebase/firestore";
+import { format } from 'date-fns';
 
 export default function Dashboard(){
   const { logout } = useContext(AuthContext);
@@ -16,49 +17,58 @@ export default function Dashboard(){
   const listRef = collection(db, 'chamados');
   const [isEmpty, setIsEmpty] = useState(false);
 
-  useEffect(() => {
-    async function loadChamados() {
-      // Define a query para buscar os chamados ordenados por data de criação em ordem decrescente, limitando a 5 resultados
+  useEffect(()=>{
+    async function loadChamados(){
       const q = query(listRef, orderBy('created', 'desc'), limit(5));
-      // Executa a query e obtém o snapshot dos resultados
       const querySnapshot = await getDocs(q);
-      // Atualiza o estado dos chamados com base no snapshot
+      setChamados([]);
       await updateState(querySnapshot);
-      setloading(false); // Define que o carregamento foi concluído
+      setloading(false);
     }
-  
-    loadChamados(); // Chama a função de carregamento de chamados
-  
-    return () => { 
-      // Função de limpeza, não realiza nenhuma ação
-    };
+
+    loadChamados();
+
+    return ()=>{ }
   }, []);
-  
-  async function updateState(querySnapshot) {
+
+  async function updateState(querySnapshot){
     const isCollectionEmpty = querySnapshot.size === 0;
-  
     if (!isCollectionEmpty) {
       let lista = [];
-      // Itera sobre os documentos no snapshot e adiciona os dados na lista
-      querySnapshot.forEach((doc) => {
+      querySnapshot.forEach((doc)=>{
         lista.push({
           id: doc.id,
           assunto: doc.data().assunto,
           cliente: doc.data().cliente,
           clienteId: doc.data().clienteId,
           created: doc.data().created,
+          createdFormat: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
           status: doc.data().status,
           complemento: doc.data().complemento
-        });
-      });
-  
-      // Atualiza o estado dos chamados, concatenando a lista atual com os novos chamados
+        })
+      })
+
       setChamados(chamados => [...chamados, ...lista]);
-    } else {
-      setIsEmpty(true); // Define que a coleção de chamados está vazia
+    }else{
+      setIsEmpty(true);
     }
   }
-  
+
+  if (loading) {
+    return(
+      <div>
+        <Header/>
+        <div>
+          <Title name='Tickets'>
+            <FiMessageSquare size={25}/>
+          </Title>
+          <div className="container dashboard">
+            <span>Buscando chamados, aguarde.</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return(
     <div>
@@ -93,24 +103,28 @@ export default function Dashboard(){
               </tr>
             </thead>
             <tBody>
-              <tr>
-                <td data-Label='Cliente'>empresa 1</td>
-                <td data-Label='Assunto'>****</td>
-                <td data-Label='Status'>
-                  <span className="badge" style={{backgroundColor:'#999'}}>
-                    Em aberto
-                  </span>
-                </td>
-                <td data-Label='Cadastrado'>****</td>
-                <td data-Label='#'>
-                  <button className="action" style={{backgroundColor: '#3583f6'}}>
-                    <FiSearch color="#fff" size={17}/>
-                  </button>
-                  <button className="action" style={{backgroundColor: '#f6a935'}}>
-                    <FiEdit2 color="#fff" size={17}/>
-                  </button>
-                </td>
-              </tr>
+              {chamados.map((item, index)=>{
+                return(
+                  <tr key={index}>
+                    <td data-Label='Cliente'>{item.cliente}</td>
+                    <td data-Label='Assunto'>{item.assunto}</td>
+                    <td data-Label='Status'>
+                      <span className="badge" style={{backgroundColor:'#999'}}>
+                        {item.status}
+                      </span>
+                    </td>
+                    <td data-Label='Cadastrado'>{item.createdFormat}</td>
+                    <td data-Label='#'>
+                      <button className="action" style={{backgroundColor: '#3583f6'}}>
+                        <FiSearch color="#fff" size={17}/>
+                      </button>
+                      <button className="action" style={{backgroundColor: '#f6a935'}}>
+                        <FiEdit2 color="#fff" size={17}/>
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
             </tBody>
           </table>
           </>
